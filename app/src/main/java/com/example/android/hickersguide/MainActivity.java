fa2453954880a5557ed94a10c3a5451fd28749f8
ahmed fadhil <fadhil@fbk.eu>
2018-01-02 17:40:38 +0100

    first commit

---
+++ app/src/main/java/com/example/android/hickersguide/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.hickersguide;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity implements LocationListener {
+    LocationManager locationManager;
+    String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(),false);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
